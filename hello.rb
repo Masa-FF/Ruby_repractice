f83fea8717d03f2e220058068637607d36058b52
Masa-FF <na7stea2@gmail.com>
2021-02-09 06:50:42 +0000

    4乗的ガウスの計算

--- hello.rb
+++ hello.rb
@@ -26,3 +26,31 @@ end
 if (hand == "グー") || (hand == "パー")
   puts "グーまたはパーを出しました。"
 end
+
+puts "求めたい数字を入力して下さい"
+puts "１つ目の数字"
+a = gets.to_i
+puts "２つ目の数字"
+b = gets.to_i
+puts "３つ目の数字"
+c = gets.to_i
+puts "何番目の数字を求めますか？"
+t = gets.to_i
+
+n = 0
+while n < (t - 3)
+  d = a + b + c
+  a = b
+  b = c
+  c = d
+  n += 1
+end
+puts "#{t}番目の数は#{c}です"
+
+s = 0
+n = 1
+while n <= 99
+  s = s + n**4
+  n = n + 1
+end
+puts s
